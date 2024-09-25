319f1a95c702c000d7dd70a2dc18ca27cccf3efc
Rüveyda Dilara Gülbaş <120405060+rvydadlra@users.noreply.github.com>
2024-09-25 15:01:49 +0300

    Update encoder_driverdx.h

--- ROSArduinoBridgedx/encoder_driverdx.h
+++ ROSArduinoBridgedx/encoder_driverdx.h
@@ -6,13 +6,13 @@
 #ifdef ARDUINO_ENC_COUNTER
  
 #define LEFT_ENC_PIN_A PD2  //pin 2
-#define LEFT_ENC_PIN_A PD3  //pin 2
-#define LEFT_ENC_PIN_A PD4  //pin 2
+#define LEFT_ENC_PIN_B PD3  //pin 2
+#define LEFT_ENC_PIN_C PD4  //pin 2
   
   //below can be changed, but should be PORTC pins
-#define RIGHT_ENC_PIN_B PC14   //pin A5
-#define LEFT_ENC_PIN_A PC15 //pin 2
-#define LEFT_ENC_PIN_A PC16  //pin 2
+#define RIGHT_ENC_PIN_A PC14   //pin A5
+#define LEFT_ENC_PIN_B PC15 //pin 2
+#define LEFT_ENC_PIN_C PC16  //pin 2
 #endif
    
 long readEncoder(int i);
